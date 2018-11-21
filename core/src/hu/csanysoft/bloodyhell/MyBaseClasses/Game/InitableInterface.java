d207bc0b2aceff03408773f307859d233ab148ed
tanulo <tuskebganz@gmail.com>
2018-11-21 16:05:28 +0100

    ASD MyBaseClasses 2

---
+++ core/src/hu/csanysoft/bloodyhell/MyBaseClasses/Game/InitableInterface.java
@@ -0,0 +1,9 @@
+package hu.csanysoft.bloodyhell.MyBaseClasses.Game;
+
+/**
+ * Created by tuskeb on 2016. 10. 29..
+ */
+
+public interface InitableInterface {
+    public void init();
+}
---
+++ core/src/hu/csanysoft/bloodyhell/MyBaseClasses/UI/MyTextField.java
@@ -0,0 +1,23 @@
+package hu.csanysoft.bloodyhell.MyBaseClasses.UI;
+
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+import hu.csanysoft.bloodyhell.MyBaseClasses.Game.InitableInterface;
+
+/**
+ * Created by tuskeb on 2016. 10. 01..
+ */
+public class MyTextField extends TextField implements InitableInterface {
+
+    public MyTextField(String text, TextFieldStyle style) {
+        super(text, style);
+        setWidth(300);
+        setHeight(50);
+        init();
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
