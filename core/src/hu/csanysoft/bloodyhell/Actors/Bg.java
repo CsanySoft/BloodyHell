3156954c793c5cfd7211ac543613261bf765393e
ErAn156 <berg.martin156@gmail.com>
2018-12-05 16:33:16 +0100

    Második pálya ütközési modelljei beállítva

--- core/src/hu/csanysoft/bloodyhell/Actors/Bg.java
+++ core/src/hu/csanysoft/bloodyhell/Actors/Bg.java
@@ -2,6 +2,7 @@ package hu.csanysoft.bloodyhell.Actors;
 
 import hu.csanysoft.bloodyhell.Global.Assets;
 import hu.csanysoft.bloodyhell.Global.Globals;
+import hu.csanysoft.bloodyhell.MyBaseClasses.Scene2D.MyCircle;
 import hu.csanysoft.bloodyhell.MyBaseClasses.Scene2D.MyRectangle;
 import hu.csanysoft.bloodyhell.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 
@@ -14,6 +15,17 @@ public class Bg extends OneSpriteStaticActor {
         addCollisionShape("Alsó kerítés bal", new MyRectangle((Globals.WORLD_WIDTH*0.7164f-Globals.WORLD_WIDTH*0.6804f)/2,Globals.WORLD_HEIGHT*0.45f,Globals.WORLD_WIDTH*0.6804f,0));
         addCollisionShape("Alsó kerítés jobb", new MyRectangle((Globals.WORLD_WIDTH*0.7164f-Globals.WORLD_WIDTH*0.6804f)/2,Globals.WORLD_HEIGHT*0.45f,Globals.WORLD_WIDTH*0.6804f + (Globals.WORLD_WIDTH*0.7164f-Globals.WORLD_WIDTH*0.6804f)/2,0));
         addCollisionShape("Ház", new MyRectangle(Globals.WORLD_WIDTH*0.225f, Globals.WORLD_HEIGHT,0,0));
+
+        /* //Második pálya collisionjai beállítva
+        addCollisionShape("Asztal", new MyRectangle((Globals.WORLD_WIDTH*0.3050f),Globals.WORLD_HEIGHT*0.2444f,Globals.WORLD_WIDTH*0.3554f,Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.60f));
+        addCollisionShape("Kanapé felső", new MyRectangle((Globals.WORLD_WIDTH*0.28f),Globals.WORLD_HEIGHT*0.1388f,Globals.WORLD_WIDTH*0.0203f,Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.2083f));
+        addCollisionShape("Kanapé alsó", new MyRectangle((Globals.WORLD_WIDTH*0.0953f),Globals.WORLD_HEIGHT*0.25f,Globals.WORLD_WIDTH*0.0203f,Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.459f));
+        addCollisionShape("Virág nagy alsó" , new MyCircle(Globals.WORLD_WIDTH*0.078f ,  8 , 5) ); // TODO: 2018. 12. 05. ha elcsúszik akkor javítani
+        addCollisionShape("Virág nagy felső" , new MyCircle(Globals.WORLD_WIDTH*0.065f ,  Globals.WORLD_WIDTH*0.6875f , Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.25f) );
+        addCollisionShape("Virág nagy felső" , new MyCircle(Globals.WORLD_WIDTH*0.065f ,  Globals.WORLD_WIDTH*0.6875f , Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.25f) );
+        addCollisionShape("Virág kicsi alsó" , new MyCircle(Globals.WORLD_WIDTH*0.0429f ,  Globals.WORLD_WIDTH*0.881f , Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.79f) );
+        addCollisionShape("Virág kicsi felső" , new MyCircle(Globals.WORLD_WIDTH*0.0429f ,  Globals.WORLD_WIDTH*0.881f , Globals.WORLD_HEIGHT - Globals.WORLD_HEIGHT*0.355f) );
+        */
     }
 
 }
